5ab2a8e0dddae109cbc6bfd827bfdab5db63598f
hsanchez <huascar.sanchez@gmail.com>
2016-10-06 13:55:23 -0700

    pattern printing: done; pattern dumping to file: now

--- kofu.rb
+++ kofu.rb
@@ -20,8 +20,221 @@ module Kofu
   LOGS    = "/log.txt"
   DEBUG   = false
   
+  ERRORED   = "errored"
+  FAILED    = "failed"
   PASSED    = "passed"
   CANCELED  = "canceled" 
+  
+  class InvalidLine < StandardError; end  
+  
+  # Travis CI status monitor
+  # monitor = Monitor.new
+  # line = ...
+  # begin
+  #   monitor.started(line)
+  #   monitor.succeded(line)
+  # rescue InvalidLine => e
+  #   monitor.failed(line)
+  # end
+  # return monitor.data
+  class Monitor
+    
+    def initialize (lang = "all", verbose = false)
+      @data     = Hash.new
+      @verbose  = verbose
+      @lang     = lang
+      @keys     = Hash.new
+      
+      if @verbose
+        @patterns = []
+        @stats    = Hash.new
+      
+        @stats[:java]  = Hash.new
+        @stats[:ruby]  = Hash.new
+      
+        @stats[:java][:count]  = 0
+        @stats[:ruby][:count]  = 0
+      
+        @stats[:java][:size] = []
+        @stats[:ruby][:size] = []
+      
+        @stats[:total] = 0
+      end
+    end
+    
+    # Public: key seen before?
+    def skip(line)
+      project = line[:gh_project_name]
+      branch  = line[:git_branch]
+    
+      key = {
+        name:   project, 
+        branch: branch
+      }
+      
+      @data.key?(key)
+    end
+    
+    # Public: starts the reading of a line  
+    def started (line)
+      
+      project = line[:gh_project_name]
+      branch  = line[:git_branch]
+      pl      = line[:gh_lang]
+      status  = line[:tr_status]
+            
+      key = {
+        name:   project, 
+        branch: branch
+      }
+            
+      # Skips the line if the travis build programming 
+      # language is different than the programming 
+      # language requested by the user. 
+      raise InvalidLine, "Skip line" if pl != @lang && @lang != "all"
+      
+      # Skips line if the status of the travis build 
+      # is a canceled status
+      raise InvalidLine, "Skip line" if status == CANCELED
+      
+      
+      if !@keys.key?(key) # not visisted key yet!        
+        
+        @keys[key] = []
+        
+        if @verbose
+          @stats[:total] += 1 # counts only not seen projects
+          
+          if @patterns.size > 1 && @patterns.last == "p"
+            puts "recorded: #{@patterns}"
+            @patterns.clear
+          end
+        end
+        
+        
+        # Skips line if the first status of the travis build
+        # is a passed status
+        raise InvalidLine, "Skip line" if status == PASSED
+        
+      else
+      
+        # Skips line if the first status of the travis build
+        # is a passed status
+        raise InvalidLine, "Skip line" if status == PASSED && @keys[key].empty?  
+      end
+            
+    end 
+    
+    # Public: succeded the reading of a line
+    def succeded(line)
+      build = Hash.new
+      
+      build[:project] = line[:gh_project_name]
+      build[:branch]  = line[:git_branch]
+      build[:lang]    = line[:gh_lang]
+      
+      key = {
+        name:   build[:project], 
+        branch: build[:branch]
+      }
+      
+      build[:build]  = Kofu.ensure_value(line[:tr_build_id])
+      build[:jobid]  = line[:tr_job_id] # useful for building log url 
+      build[:status] = line[:tr_status]
+      
+      build[:started]    = Kofu.ensure_value(line[:tr_started_at])
+      build[:commit]     = line[:git_commit]     
+          
+      build[:commiturl]  = "#{API}#{build[:project]}#{COMMITS}#{build[:commit]}"
+      build[:buildurl]   = "#{TRAVIS}#{build[:jobid]}#{LOGS}"
+      
+      if @keys[key].any?
+      
+        base  = @keys[key][-1][:commit]
+        head  = build[:commit]        
+      
+        # compare the previous commit to this new one
+        build[:patchurl] = "#{API}#{build[:project]}#{COMPARE}#{base}...#{head}"
+      else
+      
+        base          = build[:branch]
+        head          = build[:commit]        
+      
+        # compare the branch to this new commit
+        build[:patchurl] = "#{API}#{build[:project]}#{COMPARE}#{base}...#{head}"
+      end
+      
+      @keys[key].push(build) 
+            
+      if @verbose
+        @patterns.push(build[:status][0,1])
+      end      
+      
+      # stop accepting entries and create new record
+      if build[:status] == PASSED         
+        
+        if @verbose
+          @stats[:java][:count] += 1 if build[:lang] == "java"
+          @stats[:ruby][:count] += 1 if build[:lang] == "ruby"  
+        
+          if @patterns.size > 1
+            @stats[:java][:size].push(@patterns.size) if build[:lang] == "java"
+            @stats[:ruby][:size].push(@patterns.size) if build[:lang] == "ruby"
+          end
+                
+        end
+        
+        if !@data.key?(key) # lazy updating
+          @data[key] = []        
+        end
+        
+        @data[key].push(@keys[key])
+        
+      end
+      
+    end 
+    
+    def overview
+      "Nothing to report" unless @verbose
+      
+      total_recs = @stats[:total]
+      puts "Number of processed records: #{total_recs}"
+      
+      @stats.each do |k, v|
+        next if k == :total
+        next if v[:size].empty?
+        
+        if !v[:size].empty?
+              
+          puts "Number of #{k} ([errored|failed]+[passed]) patterns: #{v[:count]}"
+          puts "(Additional) details:"
+          puts "Basic stats using the size of collected patterns:"
+          
+          mean = ("%.2f" % v[:size].mean).to_f
+          min  = v[:size].min
+          max  = v[:size].max
+          stdv = v[:size].standard_deviation
+        
+          puts "min: #{min}, mean: #{mean}, max: #{max}, stdv: #{stdv}"
+        end
+      end
+      
+      
+    end
+    
+    def failed (line)      
+      if @verbose 
+        puts "ignored: #{@patterns}" if !@patterns.empty?
+      end
+      
+      line
+    end
+    
+    def data
+      @data
+    end
+  end
+  
   
   # Public: It dumps the collected records to either the screen
   # or a file named results.csv (default behavior).
@@ -43,173 +256,36 @@ module Kofu
   # @param filename name of csv file
   # Returns an array of records indexed by repository name
   def process(filename, lang, verbose = false)
-    # hash consisting of an array of arrays
-    records = Hash.new
     file    = File.join(File.dirname(__FILE__), filename)
-    
-    visited = []
-        
-    branches = Hash.new
-    
+
     unless verbose
       puts "Processing #{filename} ... This will take a few minutes."
     end
     
-    repos = Hash.new
     
-    if verbose
-      entry = []
-      stats = Hash.new
-      stats[:java]  = Hash.new
-      stats[:ruby]  = Hash.new
-      
-      stats[:java][:count]  = 0
-      stats[:ruby][:count]  = 0
-      
-      stats[:java][:size] = []
-      stats[:ruby][:size] = []
-      
-      stats[:total] = 0
-    end
-    
-    CSV.foreach(file, :headers => true, :header_converters => :symbol) do |line|      
-      build       = Hash.new
-      repository  = line[:gh_project_name]  
-      
-      build[:branch]  = line[:git_branch]
-      build[:lang]    = line[:gh_lang]      
-      
-      next if build[:lang] != lang && lang != "all"
-      
-      if verbose
-        stats[:total] += 1
-      end
-      
-      key = {
-        name:   repository, 
-        branch: build[:branch]
-      }
-      
-      # puts key
-      # print "Not seen before? #{!visited.include?(key)} == true"
-      # print ":["
-      # puts "Nil repos? #{repos[repository].nil?}"
-          
-      if !visited.include?(key) # repository
-        
-        repos[key]  = []
-        # print "Initiated"
-        # print "]"
-        # puts ""
-        
-        # Skip first status of a build if that status
-        # is a passed status
-        next if line[:tr_status] == PASSED
-        
-        visited.push(key)
-        
-        if verbose
-          if entry.size > 1 && entry.include?("p")
-            puts "#{entry}"
-            entry.clear
-          end
-        end
-      # else
-      #   print "Initiated"
-      #   print "]"
-      #   puts ""
-      end 
-      
-      # puts "Nil repos? #{repos[repository].nil?}"
-    
-      build[:build]  = Kofu.ensure_value(line[:tr_build_id])
-      build[:jobid]  = line[:tr_job_id] # useful for building log url 
-      build[:status] = line[:tr_status] 
-    
-      # Ignores status of a build if that status
-      # is a canceled status
-      next if build[:status] == CANCELED
-    
-      build[:started]    = Kofu.ensure_value(line[:tr_started_at])
-      build[:commit]     = line[:git_commit]     
-          
-      build[:commiturl]  = "#{API}#{repository}#{COMMITS}#{build[:commit]}"
-      build[:buildurl]   = "#{TRAVIS}#{build[:jobid]}#{LOGS}"
-    
-      if repos[key].any?
-      
-        base  = repos[key][-1][:commit]
-        head  = build[:commit]        
-      
-        # compare the previous commit to this new one
-        build[:patchurl] = "#{API}#{repository}#{COMPARE}#{base}...#{head}"
-      else
-      
-        base          = build[:branch]
-        head          = build[:commit]        
-      
-        # compare the branch to this new commit
-        build[:patchurl] = "#{API}#{repository}#{COMPARE}#{base}...#{head}"
-      end
-                        
-      repos[key].push(build)
-    
-      if verbose
-        entry.push(build[:status][0,1])
-      end
-          
-      if build[:status] == PASSED   
-      
-        if verbose
-          stats[:java][:count] += 1 if build[:lang] == "java"
-          stats[:ruby][:count] += 1 if build[:lang] == "ruby"  
-        
-          if entry.size > 1
-            stats[:java][:size].push(entry.size) if build[:lang] == "java"
-            stats[:ruby][:size].push(entry.size) if build[:lang] == "ruby"
-          end
-                
-        end
-             
-        visited.delete(key)
-    
-        if !records.key?(key)
-          records[key] = []        
-        end
+    monitor = Monitor.new(lang, verbose)
+            
+    CSV.foreach(file, :headers => true, :header_converters => :symbol) do |line| 
 
-        records[key].push(repos[key])
-    
-        repos.delete(key)        
+      next if monitor.skip(line)
+      
+      begin
+      
+        # preconditions checking + some var initialization
+        monitor.started(line)
+        
+        # line recording
+        monitor.succeded(line)
+      
+      rescue InvalidLine => e
+        monitor.failed(line) 
       end
       
     end
     
-    if DEBUG
-      puts "collected #{records.size} records"
-    end
+    monitor.overview
     
-    if verbose
-      total_recs = stats[:total]
-      puts "Number of processed records: #{total_recs}"
-      
-      stats.each do |k, v|
-        next if k == :total
-      
-        puts "Number of #{k} ([errored|failed]+[passed]) patterns: #{v[:count]}"
-        puts "(Additional) details:"
-        puts "Basic stats using the size of collected patterns:"
-        
-        mean = ("%.2f" % v[:size].mean).to_f
-        min  = v[:size].min
-        max  = v[:size].max
-        stdv = v[:size].standard_deviation
-        
-        puts "min: #{min}, mean: #{mean}, max: #{max}, stdv: #{stdv}"
-      end
-    end
-  
-    # [repo_0: [data, data], repo_1: [data, data], ..., repo_N: [data, data]]
-    records
+    monitor.data
   end
   
   # Internal: Prints all collected records to screen
